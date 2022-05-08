360ebaea5872b7edce18167f7a5e474066bd1696
April Hussey <april.hussey@outlook.com>
2022-05-08 20:00:31 +0100

    Current state of the menus

---
+++ TropicalEscape/Assets/Script/ButtonFunctions.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using System.Collection;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenOptions()
+    { 
+        // SceneManager.LoadScene("Options");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
