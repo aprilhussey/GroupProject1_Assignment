5b7ca2d0c97d8cbe814ee2e74d97af09b3be9afb
HollyAnna2002 <hollyhumphries2002@gmail.com>
2022-05-13 00:49:28 +0100

    Enemy + EnemyPatrol Script added
    There are some enemies in the level and they all patrol by moving across a platform and turning before each ends. They arent killable right now and they do not kill the player either

---
+++ TropicalEscape/Assets/Script/EnemyPatrol.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    private float moveDirection;
+    private float moveSpeed;
+    private Rigidbody2D r2d;
+    private bool facingRight = true;
+    private Vector3 localScale;
+
+    bool changeDirection = false;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        r2d = GetComponent<Rigidbody2D>();
+        moveDirection = -1f;
+        moveSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeDirection)
+        {
+            moveDirection *= -1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        r2d.velocity = new Vector2(moveDirection * moveSpeed, r2d.velocity.y);
+        changeDirection = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+    }
+
+    void LateUpdate()
+    {
+        Flip();
+    }
+
+    void Flip()
+    {
+        if (moveDirection > 0)
+            facingRight = true;
+        else if (moveDirection < 0)
+            facingRight = false;
+
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
+}
